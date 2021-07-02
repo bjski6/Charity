3f669f4c43714fbfc0a89809c8681291f8aa2b02
bjski6 <bjankowski06@gmail.com>
2021-07-02 18:32:59 +0200

    Manage Users, Institutions

--- src/main/java/pl/coderslab/charity/repository/DonationRepository.java
+++ src/main/java/pl/coderslab/charity/repository/DonationRepository.java
@@ -3,7 +3,10 @@ package pl.coderslab.charity.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import pl.coderslab.charity.model.Category;
 import pl.coderslab.charity.model.Donation;
+
+import java.util.List;
 
 @Repository
 public interface DonationRepository extends JpaRepository <Donation, Long> {
@@ -20,5 +23,7 @@ public interface DonationRepository extends JpaRepository <Donation, Long> {
     @Query(value = "select count(*) from donation where user_id=?1", nativeQuery = true)
     Long countUserDonation (Long id);
 
+    @Query(value = "select * from donation_categories where donation_id=?1", nativeQuery = true)
+    List<Category> categories (Long id);
 
 }
